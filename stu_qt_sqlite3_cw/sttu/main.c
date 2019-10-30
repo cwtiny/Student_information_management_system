bf89f70bc9333cbdb3cef0bcba0528d18c0819d3
hongreat <50976458+hongreat@users.noreply.github.com>
2019-10-30 16:03:28 +0800

    2019.10
    代码优化，bug修复

--- stu_qt_sqlite3_cw/sttu/main.c
+++ stu_qt_sqlite3_cw/sttu/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,9 +23,9 @@ typedef struct LNode
 
 typedef struct LSTU
 {
-    int mID;
-    char mName[20];
-    int mAge;
+    unint mID;
+    unchar mName[20];
+    unint mAge;
     double mChineseScore;
     double mMathScore;
     struct LNode *next;
@@ -33,7 +34,12 @@ typedef struct LSTU
 
 void createStuTable();
 void displayMenu();
-void Menu();
+void choice_menu_Main();
+void menu_main();
+void menu_select();
+void menu_help();
+void menu_selectByGrade();
+void menu_sortStuByGrade();
 void displayStu();
 int selectGetTable(const char *sql);
 void insertStu_ID(LNode *stu);
@@ -66,8 +72,7 @@ static int callback(void *data, int argc, char **argv, char **stuColName);
 int main()
 {
     createStuTable();
-    while(1)
-        Menu();
+    choice_menu_Main();
     return 0;
 }
 void createStuTable()
@@ -89,7 +94,7 @@ void createStuTable()
         return ;
     }
 }
-void displayMenu()
+void menu_main()
 {
     printf("\n");
     printf("****************************************************\n");
@@ -105,41 +110,91 @@ void displayMenu()
     printf("     ---------------          ---------------   \n");
     printf("请选择对应序号（0—7）:\n");
 }
-
-void Menu()
+void menu_select()
 {
-    int num;
-    displayMenu();
-    scanf("%d",&num);
+    printf("\n");
+    printf(" ***************** 查询学生信息界面*******************\n");
+    printf("Note:\n");
+    printf("     ---------------          ---------------   \n");
+    printf("     ******************************************** \n");
+    printf("     * 1.ID查询               **  2.成绩查询     * \n");
+    printf("     * 3.返回主界面           **  4.退出         *\n");
+    printf("     ********************************************\n");
+    printf("     ---------------          ---------------   \n");
+    printf("请选择对应序号（1-3）:\n");
+}
+void menu_help()
+{
+    printf("         ------------------------------\n");
+    printf("         欢迎来到学生管理系统（数据库版）\n");
+    printf("   此为完成版并非最终版，部分函数仍可继续拆分或整合\n");
+    printf("   -------------------------------------------------\n");
+    printf("\n\n\n\n\n请按任意键返回...\n");
     getchar();
-    switch (num) {
-    case 0:
-        printf("         ------------------------------\n");
-        printf("         欢迎来到学生管理系统（数据库版）\n");
-        printf("         ------------------------------\n");
-        break;
-    case 1:sortStu();
-        break;
-    case 2:selectStu();
-        break;
-    case 3:updateStu();
-        break;
-    case 4:insertStu();
-        break;
-    case 5:deleteStu();
-        break;
-    case 6:displayStu();
-        break;
-    case 7:exit(0);
-        break;
-    default:
-        printf("请选择正确的序号输入（0-7）：\n");
+    fflush(stdin);
+
+}
+void menu_selectByGrade()
+{
+    printf("1.已知语文成绩\n2.已知数学成绩\n3.返回\n");
+    printf("请输入你的选择(1-3):\n");
+}
+void menu_updateStu(LNode *stu)
+{
+    printf("选择修改ID：%s学生的信息 :\n"
+               "1.ID\n2.姓名\n3.年龄\n4.语文\n5.数学\n6.返回\n",stu->mID);
+    printf("请选择修改内容(1-6):\n");
+}
+void menu_deleteStu()
+{
+    printf("1.By学生ID:\n2.By学生姓名\n3.返回\n");
+
+    printf("请选择（1-3）:\n");
+}
+void menu_sortStu()
+{
+    printf("1.ID（升序）\n2.成绩（降序）\n3.返回\n");
+    printf("请选择（1-3）:\n");
+}
+void menu_sortStuByGrade()
+{
+    printf("1.语文成绩（降序）\n2.数学成绩（降序）\n3.总成绩（降序）\n4.总成绩（升序）\n5.返回\n");
+    printf("请选择(1-5)：\n");
+}
+void choice_menu_Main()
+{
+    int choice=8;
+    menu_main();
+    while (1) {
+        scanf("%d",&choice);
+        getchar();
+        switch (choice) {
+            case 0:menu_help();menu_main();
+                break;
+            case 1:sortStu();menu_main();
+                break;
+            case 2:selectStu();menu_main();
+                break;
+            case 3:updateStu();menu_main();
+                break;
+            case 4:insertStu();menu_main();
+                break;
+            case 5:deleteStu();menu_main();
+                break;
+            case 6:displayStu();menu_main();
+                break;
+            case 7:exit(0);
+                break;
+            default:
+                printf("请选择正确的序号输入（0-7）：\n");
+            }
     }
 }
 void displayStu()
 {
     const char *select_query="select * from stu";
-    selectGetTable(select_query);
+    if(!selectGetTable(select_query))
+        printf("显示完成\n");
 
     return;
 
@@ -148,7 +203,7 @@ void displayStu()
 void insertStu_ID(LNode *stu)
 {
     do{
-        printf("ID:\n");
+        printf("学生ID:\n");
         scanf("%s",stu->mID);
     }while(!isdigit(*stu->mID));
 
@@ -180,10 +235,10 @@ void insertStu_Chinese(LNode *stu, LSTU *realStu)
 void insertStu_Math(LNode *stu, LSTU *realStu)
 {
     do{
-        printf("数学(0-100):\n");
+        printf("数学成绩(0-100):\n");
         scanf("%s",&stu->mMathScore);
         realStu->mMathScore=atof((const char *)&stu->mMathScore);
-    }while(!isdigit(stu->mChineseScore)||realStu->mMathScore<0||realStu->mMathScore>100);
+    }while(!isdigit(stu->mMathScore)||realStu->mMathScore<0||realStu->mMathScore>100);
 }
 void insertStu_All(LNode *stu, LSTU *realStu)
 {
@@ -201,11 +256,13 @@ void insertStu()
     int i=1;
 
     while(1){
+
         printf("请输入第%d名学生信息：\n",i);
         insertStu_All(&stu,&realStu);
         strcpy(insertStr,"insert into stu values(");
         sprintf(insertStr,"%s %s,'%s',%d,%lf,%lf);",insertStr,stu.mID,stu.mName,realStu.mAge,realStu.mChineseScore,realStu.mMathScore);
         printf("%s\n",insertStr);
+
         if(!selectGetTable(insertStr))
         printf("\n第%d名学生信息添加成功！\n",i);
         else
@@ -228,29 +285,21 @@ void insertStu()
 
 void selectStu()
 {
-    int choice;
-    printf("1.ID查询\n");
-    printf("2.成绩查询\n");
-    printf("3.返回\n");
-    printf("请输入你的选择:\n");
-    choice=getchar();
-    getchar();
-
-    while (choice>'3'||choice<'1')
-    {
-        printf("请输入正确的选择（1-3）\n");
+    int choice=0;
+    menu_select();
+    while (1) {
+        scanf("%d",&choice);
         getchar();
-        choice=getchar();
 
-    }
-    switch (choice) {
-    case '1':selectByID();
-        break;
-    case '2':selectByGrade();
-        break;
-    case '3':return;
-    default:
-        break;
+        switch (choice) {
+        case 1:selectByID();menu_select();
+            break;
+        case 2:selectByGrade();menu_select();
+            break;
+        case 3:return;
+        default:
+            printf("请选择正确的序号输入（1-3）：\n");
+        }
     }
 }
 
@@ -259,68 +308,61 @@ void selectByID()
     LNode stu;
     char selectSql[100]="select * from stu where id = ";
 
-    do
-    {
-        printf("输入学生ID:\n");
-        scanf("%s",stu.mID);
-    }while(!isdigit(*stu.mID));
+    insertStu_ID(&stu);
 
     sprintf(selectSql,"%s%s;",selectSql,stu.mID);
     printf("%s\n",selectSql);
-    selectGetTable(selectSql);
+    if(!selectGetTable(selectSql))
+        printf("操作成功\n");
 }
+
 void selectByGrade()
-{
+{ 
     int choice;
-    printf("1.已知语文成绩\n2.已知数学成绩\n3.返回\n");
-    do{
-        printf("请输入你的选择:\n");
+        menu_selectByGrade();
+        while(1){
         scanf("%d",&choice);
         getchar();
-    }while(choice<1||choice>3);
-    switch (choice) {
-    case 1:selectByChinese();break;
-    case 2:selectByMath();break;
-    case 3:return;
-    default:
-        break;
+
+        switch (choice) {
+        case 1:selectByChinese();menu_selectByGrade();
+            break;
+        case 2:selectByMath();menu_selectByGrade();
+            break;
+        case 3:return;
+        default:
+            printf("请选择正确的序号输入（1-3）：\n");
+        }
     }
+
 }
 void selectByChinese()
 {
     LNode stu;
+    LSTU realStu;
     char selectSql[100]="select * from stu where Chinese = ";
-    double chinese;
 
-    do{
-        printf("请输入语文成绩 (0-100):\n");
-        scanf("%s",&stu.mChineseScore);
-        //getchar();
-        chinese = atof((const char*)&stu.mChineseScore);
-    }while(!isdigit(stu.mChineseScore)||chinese<0||chinese>100);
+    insertStu_Chinese(&stu, &realStu);
 
-    sprintf(selectSql,"%s%lf;",selectSql,chinese);
+    sprintf(selectSql,"%s%lf;",selectSql,realStu.mChineseScore);
     printf("%s\n",selectSql);
-    selectGetTable(selectSql);
+    if(!selectGetTable(selectSql))
+        printf("操作成功\n");
 
 }
 
 void selectByMath()
 {
     LNode stu;
+    LSTU realStu;
     char selectSql[100]="select * from stu where math = ";
-    double math;
 
-    do{
-        printf("请输入数学成绩(0-100):\n");
-        scanf("%s",&stu.mMathScore);
-        //getchar();
-        math = atof((const char*)&stu.mMathScore);
-    }while(!isdigit(stu.mMathScore)||math<0||math>100);
+    insertStu_Math(&stu, &realStu);
 
-    sprintf(selectSql,"%s%lf;",selectSql,math);
+    sprintf(selectSql,"%s%lf;",selectSql,realStu.mMathScore);
     printf("%s\n",selectSql);
-    selectGetTable(selectSql);
+    if(!selectGetTable(selectSql))
+        printf("操作成功\n");
     //select_exec(selectSql);
 }
 
@@ -330,206 +372,187 @@ void updateStu()
 {
     LNode stu;
     int choice;
-    do{
-    printf("学生ID:\n");
-    scanf("%s",stu.mID);
-    //getchar();
-    }while (!isdigit(*stu.mID));
+    insertStu_ID(&stu);
+    menu_updateStu(&stu);
     while (1){
-        printf("选择修改ID：%s学生的信息 :\n"
-               "1.ID\n2.姓名\n3.年龄\n4.语文\n5.数学\n6.返回\n",stu.mID);
-        do
-        {
-            printf("请选择修改内容:\n");
-            scanf("%d",&choice);
-            getchar();
-        }while(choice<1||choice>6);
-
+        scanf("%d",&choice);
+        getchar();
         switch (choice) {
-        case 1:updateID(&stu);
+        case 1:updateID(&stu);menu_updateStu(&stu);
             break;
-        case 2:updateName(&stu);
+        case 2:updateName(&stu);menu_updateStu(&stu);
             break;
-        case 3:updateAge(&stu);
+        case 3:updateAge(&stu);menu_updateStu(&stu);
             break;
-        case 4:updateChinese(&stu);
+        case 4:updateChinese(&stu);menu_updateStu(&stu);
             break;
-        case 5:updateMath(&stu);
+        case 5:updateMath(&stu);menu_updateStu(&stu);
             break;
+        case 6:return;
         default:
-            return;
+            printf("请选择正确的序号输入（1-6）：\n");
         }
     }
 
 }
 void updateID(LNode *stu)
 {
-    unchar newID[20];
+    LNode newStu;
     char updateSql[100]="update stu set ";
-    do{
-        printf("修改为:\n");
-        scanf("%s",newID);
-        //getchar();
-    }while (!isdigit(*newID));
-    sprintf(updateSql,"%s id = %d where id = %d;",updateSql,atoi((const char*)newID),atoi((const char*)stu->mID));
+    printf("修改ID为%s为新\n",stu->mID);
+    insertStu_ID(&newStu);
+    sprintf(updateSql,"%s id = %d where id = %d;",updateSql,atoi((const char*)newStu.mID),atoi((const char*)stu->mID));
     printf("%s\n",updateSql);
-    selectGetTable(updateSql);
+    if(!selectGetTable(updateSql))
+        printf("修改成功！\n");
+    else
+        printf("修改ID失败，请校验ID是否重复\n");
     //strcpy(stu->mID,newID);
-    printf("修改成功！\n");
 
 }
 void updateName(LNode *stu)
 {
     char updateSql[100]="update stu set ";
 
-    printf("修改为:\n");
-    scanf("%s",stu->mName);
-    //getchar();
+    printf("修改ID为%s学生姓名为新\n",stu->mID);
+    insertStu_Name(stu);
     sprintf(updateSql,"%s name = '%s' where id = %d;",updateSql,stu->mName,atoi((const char*)stu->mID));
 
     printf("%s\n",updateSql);
-    selectGetTable(updateSql);
-    printf("修改成功!\n");
+    if(!selectGetTable(updateSql))
+        printf("修改成功!\n");
+
 }
 void updateAge(LNode *stu)
 {
     char updateSql[100]="update stu set ";
-    unchar age;
+    LSTU realStu;
+    printf("修改ID为%s学生年龄为新\n",stu->mID);
+    insertStu_Age(stu,&realStu);
 
-    do{
-        printf("修改年龄为(1-100):\n");
-        scanf("%s",&stu->mAge);
-       // getchar();
-        age = atoi((const char*)&stu->mAge);
-    }while (!isdigit(stu->mAge)||age<=0||age>=100);
-
-    sprintf(updateSql,"%s age = %d where id = %d;",updateSql,age,atoi((const char*)stu->mID));
+    sprintf(updateSql,"%s age = %d where id = %d;",updateSql,realStu.mAge,atoi((const char*)stu->mID));
 
     printf("%s\n",updateSql);
-    selectGetTable(updateSql);
-    printf("修改成功!\n");
+    if(!selectGetTable(updateSql))
+        printf("修改成功!\n");
 }
 void updateChinese(LNode *stu)
 {
     char updateSql[100]="update stu set ";
-    double chinese;
+    LSTU realStu;
+    printf("修改ID为%s学生语文成绩为新\n",stu->mID);
+    insertStu_Chinese(stu,&realStu);
 
-    do{
-        printf("修改为（0-100）:\n");
-        scanf("%s",&stu->mChineseScore);
-       // getchar();
-        chinese = atof((const char*)&stu->mChineseScore);
-    }while(!isdigit(stu->mChineseScore)||chinese<0||chinese>100);
-
-    sprintf(updateSql,"%s Chinese = %lf where id = %d;",updateSql,chinese,atoi((const char*)stu->mID));
+    sprintf(updateSql,"%s Chinese = %lf where id = %d;",updateSql,realStu.mChineseScore,atoi((const char*)stu->mID));
     printf("%s\n",updateSql);
-    selectGetTable(updateSql);
-    printf("修改成功!\n");
+    if(!selectGetTable(updateSql))
+        printf("修改成功!\n");
 }
 void updateMath(LNode *stu)
 {
     char updateSql[100]="update stu set ";
-    double math;
+    LSTU realStu;
+    printf("修改ID为%s学生数学成绩为新\n",stu->mID);
+    insertStu_Math(stu,&realStu);
 
-    do{
-        printf("修改为(0-100):\n");
-        scanf("%s",&stu->mMathScore);
-       // getchar();
-        math = atof((const char*)&stu->mMathScore);
-    }while(!isdigit(stu->mMathScore)||math<0||math>100);
-
-    sprintf(updateSql,"%s math = %lf where id = %d;",updateSql,math,atoi((const char*)stu->mID));
+    sprintf(updateSql,"%s math = %lf where id = %d;",updateSql,realStu.mMathScore,atoi((const char*)stu->mID));
 
     printf("%s\n",updateSql);
-    selectGetTable(updateSql);
-    printf("修改成功!\n");
+    if(!selectGetTable(updateSql))
+        printf("修改成功!\n");
 }
 void deleteStuByID()
 {
     LNode stu;
     char deleteSql[100]="delete from stu where id = ";
-
-    do{
-        printf("请输入要删除的学生ID:\n");
-        scanf("%s",stu.mID);
-    }while(!isdigit(*stu.mID));
+    printf("请输入要删除的学生ID\n");
+    insertStu_ID(&stu);
 
     sprintf(deleteSql,"%s%d;",deleteSql,atoi((const char*)stu.mID));
 
-    selectGetTable(deleteSql);
-    printf("删除成功!\n");
+    if(!selectGetTable(deleteSql))
+        printf("删除成功!\n");
 
 }
 void deleteStuByName()
 {
     LNode stu;
     char deleteSql[100]="delete from stu where name =";
-    printf("请输入要删除的学生姓名:\n");
-    scanf("%s",stu.mName);
+    printf("请输入要删除的学生姓名\n");
+    insertStu_Name(&stu);
     sprintf(deleteSql,"%s'%s';",deleteSql,stu.mName);
-    selectGetTable(deleteSql);
-    printf("delete succeed\n");
+    if(!selectGetTable(deleteSql))
+        printf("删除成功\n");
     return;
 }
+
 void deleteStu()
 {
     int choice;
-    printf("1.By学生ID:\n2.By学生姓名\n3.返回\n");
-    do{
-        printf("请选择（1-3）:\n");
+    menu_deleteStu();
+    while (1){
         scanf("%d",&choice);
         getchar();
-    }while (choice<=0||choice>3);
-    switch(choice){
-    case 1 : deleteStuByID();break;
-    case 2 : deleteStuByName();break;
-    case 3 : return;
+
+        switch(choice){
+        case 1 : deleteStuByID();menu_deleteStu();
+            break;
+        case 2 : deleteStuByName();menu_deleteStu();
+            break;
+        case 3 : return;
+        default:
+            printf("请选择正确的序号输入（1-3）：\n");
+        }
     }
+
 }
+
 void sortStu()
 {
     int choice;
-    printf("1.ID（升序）\n2.成绩（降序）\n3.返回\n");
-    do{
-        printf("请选择（1-3）:\n");
+    menu_sortStu();
+    while (1){
         scanf("%d",&choice);
         getchar();
-    }while(choice<1||choice>3);
 
-    switch (choice) {
-    case 1:sortStuMethod("id","ASC");
-        break;
-    case 2:sortStuByGrade();
-        break;
-    default:
-        break;
+        switch (choice) {
+            case 1:sortStuMethod("id","ASC");menu_sortStu();
+                break;
+            case 2:sortStuByGrade();menu_sortStu();
+                break;
+            case 3:return;
+            default:
+                printf("请选择正确的序号输入（1-3）：\n");
+        }
     }
+
 }
+
 
 void sortStuByGrade()
 {
     int choice;
-    printf("1.语文成绩（降序）\n2.数学成绩（降序）\n3.总成绩（降序）\n4.总成绩（升序）\n5.返回\n");
-    do{
-        printf("请选择(1-5)：\n");
+    menu_sortStuByGrade();
+    while (1){
         scanf("%d",&choice);
         getchar();
-    }while(choice<1||choice>5);
 
-    switch (choice) {
-    case 1:sortStuMethod("Chinese","DESC");
-        break;
-    case 2:sortStuMethod("math","DESC");
-        break;
-    case 3:sortStuBySum("id,name,sum(Chinese+math)","id","order by sum(Chinese+math) DESC");
-        break;
-    case 4:sortStuBySum("id,name,sum(Chinese+math)","id","order by sum(Chinese+math) ASC");
-        break;
-    case 5:return;
-        break;
-    default:
-        break;
+        switch (choice) {
+            case 1:sortStuMethod("Chinese","DESC");menu_sortStuByGrade();
+                break;
+            case 2:sortStuMethod("math","DESC");menu_sortStuByGrade();
+                break;
+            case 3:sortStuBySum("id,name,sum(Chinese+math)","id","order by sum(Chinese+math) DESC");menu_sortStuByGrade();
+                break;
+            case 4:sortStuBySum("id,name,sum(Chinese+math)","id","order by sum(Chinese+math) ASC");menu_sortStuByGrade();
+                break;
+            case 5:return;
+                break;
+            default:
+                printf("请选择正确的序号输入（0-5）：\n");
+        }
     }
+
 }
 void sortStuMethod(const char * method,const char *ascOr )
 {
@@ -608,6 +631,10 @@ int selectGetTable(const char *sql)
 
     sqlite3_close(db);
     db=NULL;
+    printf("\n\n请按任意键返回...");
+    fflush(stdin);
+    getchar();
+    fflush(stdin);
     return 0;
 }
 static int callback(void *data, int argc,char **argv,char **stuColName)
@@ -620,7 +647,10 @@ static int callback(void *data, int argc,char **argv,char **stuColName)
     {
         printf("%s : %s\n",stuColName[i],argv[i]?argv[i]:"NULL");
     }
-    printf("\n");
+    printf("\n\n请按任意键返回...");
+    fflush(stdin);
+    getchar();
+    fflush(stdin);
     return 0;
 }
 
